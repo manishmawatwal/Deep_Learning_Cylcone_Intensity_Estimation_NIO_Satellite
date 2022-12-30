ba80be5f9b83882035dbb3c28b8f01b87192f285
Manish <56023675+manishmawatwal@users.noreply.github.com>
2022-12-30 21:48:36 +0530

    Add files via upload

--- miscellaneous_code/confusion_matrix.py
+++ miscellaneous_code/confusion_matrix.py
@@ -1,18 +1,26 @@
-val_generator.reset()
-val_predictions = xception_model.predict(val_generator)
-
 from sklearn.metrics import confusion_matrix
 
+val_generator.reset()
+
+label_map = dict((v, k) for k, v in train_generator.class_indices.items())
+label_map
+
+actualLables_val = [label_map[k] for k in val_generator.classes]
+print("Length of actual validation labels:", len(actualLables_val))
+
 # Generate predictions for the validation data
-val_predictions = xception_model.predict(val_generator)
+val_predictions = xception_model.predict(val_generator, verbose=1)
 
 # Convert the predictions to a one-hot encoded format
 val_predictions = np.argmax(val_predictions, axis=1)
-
-# Convert the labels to a one-hot encoded format
-val_labels = np.argmax(val_generator.labels, axis=1)
+predictedLables_val = [label_map[k] for k in val_predictions]
 
 # Calculate the confusion matrix
-confusion_matrix = confusion_matrix(val_labels, val_predictions)
-
+confusion_matrix = confusion_matrix(actualLables_val, predictedLables_val)
 print(confusion_matrix)
+
+print("Filename \t\t\t Actual \t Predicted")
+for i in range(len(actualLables_val)):
+    print(actualLables_val[i] + '\t ' + predictedLables_val[i])
+
+
